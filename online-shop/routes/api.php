8977b8a034afea0b71acd70703181b67e24f6e19
OHOUD ALHEFEITI <36378728+El3en@users.noreply.github.com>
2023-01-27 19:21:52 +0300

    Add files via upload

--- online-shop/routes/api.php
+++ online-shop/routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\api\AuthController;
-use App\Http\Controllers\api\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('login', [AuthController::class, 'login']);
 
