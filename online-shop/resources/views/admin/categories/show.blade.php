49f2b4f4c101dc038ddbd44e605da0fdacf989b0
OHOUD ALHEFEITI <36378728+El3en@users.noreply.github.com>
2023-01-27 20:06:08 +0400

    Update show.blade.php

--- online-shop/resources/views/admin/categories/show.blade.php
+++ online-shop/resources/views/admin/categories/show.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.admin')
 @section('content')
     <h2>Show Category</h2>
-    
-        <label>Name</label>
-        <h3>{{$category->name}}</h3>
-        <img src="{{asset('storage/'.$category->image)}}" />
-        <a class="btn btn-secondary" href="{{ url('admin/categories') }}">Cancel</a>
-    </form>
+    <label>Name</label>
+    <h3>{{ $category->name }}</h3>
+    <hr>
+
+    <label>Image</label><br>
+    <img src="{{ asset('storage/' . $category->image) }}" />
+    <br/>
+    <hr>
+    <a class="btn btn-secondary" href="{{ url('admin/categories') }}">Back</a>
 @endsection
